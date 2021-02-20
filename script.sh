7536233149fc9a577699c66b9b5247df8c11719e
天心 <frost@tianxindeiMac-Pro.local>
2021-02-20 12:44:04 +0800

    update

--- script.sh
+++ script.sh
@@ -14,10 +14,10 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp
 mv -f package-temp/ package/lean/luci-app-adguardhome
 rm -rf package-temp
 # Add luci-theme-opentomcat
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
-rm -rf theme-temp/luci-theme-opentomcat/LICENSE
-rm -rf theme-temp/luci-theme-opentomcat/README.md
-mv -f theme-temp/luci-theme-opentomcat package/lean/
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  theme-temp/luci-theme-argon
+rm -rf theme-temp/luci-theme-argon/LICENSE
+rm -rf theme-temp/luci-theme-argon/README.md
+mv -f theme-temp/luci-theme-argon package/lean/
 rm -rf theme-temp
-default_theme='opentomcat'
+default_theme='argon'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
