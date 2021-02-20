40e868c91b70723553dc6fee00122ec4dd0a25f5
天心 <frost@tianxindeiMac-Pro.local>
2021-02-20 13:09:09 +0800

    update

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "clone adguard home"
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp
 mv -f package-temp/ package/lean/luci-app-adguardhome
 rm -rf package-temp
-# Add luci-theme-opentomcat
+# Add luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  theme-temp/luci-theme-argon
 rm -rf theme-temp/luci-theme-argon/LICENSE
@@ -21,5 +21,4 @@ rm -rf theme-temp/luci-theme-argon/README.md
 rm -rf package/lean/luci-theme-argon
 mv -f theme-temp/luci-theme-argon package/lean/
 rm -rf theme-temp
-default_theme='argon'
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
